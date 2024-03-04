108234bdc00bd48270c67a5bac1b1deb8925b70c
SATiYA <satiya@MACBOOKs-MacBook-Pro.local>
2024-03-04 14:33:56 +0700

    update query process 2

--- app/Http/Controllers/process.php
+++ app/Http/Controllers/process.php
@@ -15,8 +15,7 @@ class process extends Controller
         $bent = DB::select("SELECT DISTINCT pttype , ptname , ben_pttype , ben_ptname
                 FROM claim_list
                 LEFT JOIN benefit ON benefit.ben_pttype = claim_list.pttype
-                WHERE benefit.ben_pttype IS NULL
-                AND hcode = {$hcode}");
+                WHERE hcode = {$hcode}");
         //  dd($bent);
         $map = DB::table('benefit')->where('ben_hcode',$hcode)->orderBy('ben_status_id','ASC')->get();
         $op_paid = DB::table('claim_paid')->get();
